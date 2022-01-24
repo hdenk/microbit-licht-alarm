6a6066017b240471994e017e6f65d6d68dab37a6
hdenk <68135+hdenk@users.noreply.github.com>
2022-01-24 21:37:12 +0100

    make volume controllable

--- main.ts
+++ main.ts
@@ -1,18 +1,37 @@
-music.setVolume(60)
+input.onButtonPressed(Button.A, function () {
+    if (lautstaerke >= 0) {
+        lautstaerke = lautstaerke - 1
+        music.setVolume(lautstaerke)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (lautstaerke <= 255) {
+        lautstaerke = lautstaerke + 1
+        music.setVolume(lautstaerke)
+    }
+})
+function unerlaubteFrequenz (freq: number) {
+    return freq < MIN_TON_FREQ || freq > MAX_TON_FREQ
+}
+let lautstaerke = 0
+let MAX_TON_FREQ = 0
+let MIN_TON_FREQ = 0
+let LAUSTAERKE_BEI_START = 60
 let ALARM_SCHWELLE_LICHT = 100
-let MIN_TON_FREQ = 262
-let MAX_TON_FREQ = 523
-let TON_SCHRITTE = 32
+MIN_TON_FREQ = 262
+MAX_TON_FREQ = 440
+let TON_SCHRITTE = 12
 let freqDelta = (MAX_TON_FREQ - MIN_TON_FREQ) / TON_SCHRITTE
-let tonFreq = MIN_TON_FREQ
+let alarmTonFrequenz = MIN_TON_FREQ
+lautstaerke = LAUSTAERKE_BEI_START
+music.setVolume(lautstaerke)
 basic.forever(function () {
     if (input.lightLevel() > ALARM_SCHWELLE_LICHT) {
-        if (Math.round(tonFreq) < MAX_TON_FREQ) {
-            tonFreq += freqDelta
-            music.playTone(Math.round(tonFreq), music.beat(BeatFraction.Half))
-            music.rest(music.beat(BeatFraction.Sixteenth))
-        } else {
-            tonFreq = MIN_TON_FREQ
+        if (unerlaubteFrequenz(alarmTonFrequenz)) {
+            freqDelta = freqDelta * -1
         }
+        music.playTone(alarmTonFrequenz, music.beat(BeatFraction.Eighth))
+        alarmTonFrequenz = alarmTonFrequenz + freqDelta
     }
+    basic.pause(1)
 })
